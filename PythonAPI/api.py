5b41c2fcf08e974e03f7fe8256f422bb385a4c51
Raji Navarro <rajinavarro09@gmail.com>
2019-10-15 10:20:17 -0300

    First Commit

--- PythonAPI/api.py
+++ PythonAPI/api.py
@@ -20,7 +20,16 @@ def home():
 @app.route('/all_teams/<string:team>', methods=['GET'])
 def team_per_name(team):
     #team_per_name = [dev for dev in all_teams if dev['team'] == team]
-    
+    for i in all_teams:
+        
+        print(team, all_teams[i][team])
+        
+
+    '''    if team == all_teams[i][team]:
+            print(team, all_teams[i][team])
+            team_per_name2 = team
+    '''
+    #print(f"AQUI: {all_teams}")
     return jsonify(all_teams[i][team]), 200
 
 if __name__ == '__main__':
